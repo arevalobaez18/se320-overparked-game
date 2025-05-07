c2a5c0cbbd0ba71d39be49e565a9d9f6a5615c24
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-05-06 19:49:55 -0700

    Update ParkingApiRequestManager.cs

--- Assets/Scripts/ParkingApiRequestManager.cs
+++ Assets/Scripts/ParkingApiRequestManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [System.Serializable]
 partial class JsonParkingStructure
@@ -24,5 +25,29 @@ public class ParkingApiRequestManager : MonoBehaviour
     {
         // TODO
         return Array.Empty<JsonParkingStructure>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(GetRequest("https://webfarm.chapman.edu/parkinginfo/"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError("Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log("Response: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
     }
 }
