aac165fbd71c38a3a9a5ad545fd94f6c2f67d8d9
Brendan Chen <me@bchen.dev>
2025-05-06 18:24:08 -0700

    add a request manager script

--- Assets/Scripts/JsonParkingStructure.cs
+++ Assets/Scripts/JsonParkingStructure.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityProgressBar;
 
-public class ParkingStructure : MonoBehaviour
+public class JsonParkingStructure : MonoBehaviour
 {
     [Header("Data")]
     [SerializeField]
---
+++ Assets/Scripts/ParkingApiRequestManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+partial class JsonParkingStructure
+{
+    public string address;
+    public int capacity;
+    public int currentCount;
+    public new string name;
+}
+
+[System.Serializable]
+class JsonParkingResponse
+{
+    public JsonParkingStructure[] parkingStructures;
+}
+
+public class ParkingApiRequestManager : MonoBehaviour
+{
+
+}
